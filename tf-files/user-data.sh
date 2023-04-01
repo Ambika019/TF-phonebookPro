821226de3c5dc1309bd858dc7addb5456646420f
Ambika019 <121255312+Ambika019@users.noreply.github.com>
2023-03-31 20:27:55 -0500

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="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"
+TOKEN="XXXXXX"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/Ambika019/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
